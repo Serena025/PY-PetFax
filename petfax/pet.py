39cc117eb48886591b6aa47fa1239e7dc6d28e47
“JonsCodes” <johnalyashai@me.com>
2023-09-23 15:24:34 -0700

    Working on new routes

--- petfax/pet.py
+++ petfax/pet.py
@@ -9,3 +9,7 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 @bp.route('/')
 def index(): 
     return render_template('index.html', pets=pets)
+
+def show_pet(index):
+    pet = pets[index]  # Assuming `pets` is the list of pet data
+    return render_template('show_pet.html', pet=pet)
