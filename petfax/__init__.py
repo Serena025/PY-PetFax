c322b892947f37aebd4c8648379ca0c5e0842945
“JonsCodes” <johnalyashai@me.com>
2023-09-23 18:40:56 -0700

    Completed Static/Styling

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,6 +1,7 @@
 # config
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
 
@@ -12,6 +13,10 @@ def create_app():
     # register pet blueprint
     from . import pet
     app.register_blueprint(pet.bp)
+
+    # register fact blueprint
+    from . import fact
+    app.register_blueprint(fact.bp)
 
     # return the app
     return app
